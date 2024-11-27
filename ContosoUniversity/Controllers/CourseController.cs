3f522cf632cddee1da416e31efc0656780c7043d
opilane <opilane@HP-CZC321706M>
2024-11-27 15:39:03 +0200

    yrthrtyhr

--- ContosoUniversity/Controllers/CourseController.cs
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -86,31 +86,7 @@ namespace ContosoUniversity.Controllers
         }
 
 
-        public async Task<IActionResult> Clone(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var course = await _context.Courses.FindAsync(id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-
-            var biggestCourseId = _context.Courses.OrderByDescending(s => s.CourseID).First();
-
-            var clonedCourse = new Course
-            {
-                CourseID = biggestCourseId.CourseID + 1,
-                Title = course.Title,
-                Credits = course.Credits,
-            };
-
-            _context.Add(clonedCourse);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+        
         [HttpGet]
         public async Task<IActionResult> Create()
         {
